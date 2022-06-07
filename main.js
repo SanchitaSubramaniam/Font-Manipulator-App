f20de4fc98feedb2770b4ef7bf70dbe3c541bbf5
SanchitaSubramaniam <86506500+SanchitaSubramaniam@users.noreply.github.com>
2022-06-07 17:43:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var leftWristX=0;
+var rightWristX=0;
+var difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -13,14 +17,23 @@ function modelLoaded() {
     console.log('PoseNet Is Initialized!');
 }
 
-function draw() {
-    background('#FF6347');
-}
-
 function gotPoses(results)
 {
+
     if(results.length > 0)
     {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
     }
+}
+
+function draw() {
+    background('#FF6347');
+    document.getElementById("font_size").innerHTML = "Font Size of the Text will be " + difference + "px";
+    textSize(difference);
+    fill('FFE787');
+    text('Sanchita', 50, 400);
 }
